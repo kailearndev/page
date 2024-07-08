1cc10dc3d57fb3ebeed71e8cf55c01941a80c1f2
BM Kai <bm.hcmc.kai@gmail.com>
2024-07-08 13:40:33 +0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import "./App.css";
 import { About } from "./components/About";
 import { Cta } from "./components/Cta";
 import { FAQ } from "./components/FAQ";
 import { Features } from "./components/Features";
-import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
@@ -13,7 +13,6 @@ import { Services } from "./components/Services";
 import { Sponsors } from "./components/Sponsors";
 import { Team } from "./components/Team";
 import { Testimonials } from "./components/Testimonials";
-import "./App.css";
 
 function App() {
   return (
@@ -31,7 +30,7 @@ function App() {
       <Pricing />
       <Newsletter />
       <FAQ />
-      {/* <Footer /> */}
+
       <ScrollToTop />
     </>
   );
